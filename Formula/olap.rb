6560478e23215e889ca55dd497a1fb235966edf3
Shubham Takankhar <shubham@checkred.com>
2026-06-13 17:58:35 +0530

    chore: bump olap to 0.3.0

--- Formula/olap.rb
+++ Formula/olap.rb
@@ -1,8 +1,8 @@
 class Olap < Formula
   desc "Orchestrated loop architect/worker CLI with a Pi-powered TUI"
   homepage "https://github.com/geekyshubham/olap"
-  url "https://registry.npmjs.org/@geekyshubham/olap/-/olap-0.2.0.tgz"
-  sha256 "1f607472581997ed17a86196997280642abe38b4fe9decf834bc3d065a9be159"
+  url "https://registry.npmjs.org/@geekyshubham/olap/-/olap-0.3.0.tgz"
+  sha256 "d156b368cf6682518854f037fd9b4690a29e4e3755cdf45ffa4bfbe418380669"
   license "MIT"
 
   depends_on "node@22"
